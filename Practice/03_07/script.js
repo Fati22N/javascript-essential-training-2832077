cc86fb162c06695d16d40cc236341a6aa75f9f33
Fati22N <156519718+Fati22N@users.noreply.github.com>
2024-03-26 15:31:11 +0330

    Update script.js

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,24 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+const LapTop = {
+  name: "Asus",
+  color: "Black",
+  CPU: "core i7",
+  OS: "Linux-Ubuntu",
+  generation: 9,
+  size: {
+    height: 30,
+    length: 50
+  },
+  lidOpen: true,
+  status: function(lidStatus){
+  this.lidOpen= lidStatus;
+  },
+  newSize: function (newhieght,newlength){
+    this.size = newhight,
+      this.size = newlength;
+  },
+};
+console.log("this is laptop:" , LapTop);
+console.log("this is size:" , LapTop.size.height);
